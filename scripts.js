345d73da6c12bc481eeebb5fa940740e1e9d3c71
Axel Valverde Sanchís <axelvalverdesanchis@MacBook-Pro-de-Axel.local>
2024-10-14 23:17:30 +0200

    actualizacion diseño

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
-/*var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#navbar-example2'
-  })
-var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
-  dataSpyList.forEach(function (dataSpyEl) {
-    bootstrap.ScrollSpy.getInstance(dataSpyEl)
-      .refresh()
-  })*/
+const element = document.querySelector('.navbar-toggler');
+const boton = document.querySelector('#botonmenu');
+element.addEventListener('click', event => {
+  if(boton.classList.contains("btn-close")){
+    boton.classList.replace('btn-close','navbar-toggler-icon');
+  }else{
+    boton.classList.replace('navbar-toggler-icon', 'btn-close');
+  }
+  
+});
